4b2d31f620d09704c40c10402bc6ab5524b6cee1
Tan Chun Wei <tanchunwei@gmail.com>
2017-03-04 12:45:31 +0800

    config procfile

--- example.py
+++ example.py
@@ -3,9 +3,12 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route("/")
-	def getIpPair():
-		spider = IP_Spider()
-		ip = spider.generate_ip_pool()
-		#return jsonify(ip)
-		return "HelloWorld"
+def getIpPair():
+	spider = IP_Spider()
+	ip = spider.generate_ip_pool()
+	#return jsonify(ip)
+	return "HelloWorld"
+	
+if __name__ == '__main__':
+    app.run()
 	
